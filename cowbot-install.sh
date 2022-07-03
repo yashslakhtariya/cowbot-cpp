03b13e79cbc85148d51931c8bf502b9826907131
Yash S Lakhtariya <98678102+yashslakhtariya@users.noreply.github.com>
2022-07-03 22:16:19 +0530

    Update cowbot-install.sh

--- cowbot-install.sh
+++ cowbot-install.sh
@@ -1,7 +1,7 @@
-chmod +x ~/cowbot/src/cowbot
-chmod +x ~/cowbot/src/cowbot-help
-chmod +x ~/cowbot/src/cowbot-setup
-sudo mv ~/cowbot/src/* /usr/bin/
+chmod +x ../src/cowbot
+chmod +x ../src/cowbot-help
+chmod +x ../src/cowbot-setup
+sudo mv ../src/* /usr/bin/
 sudo mkdir /usr/share/cowbot
-sudo mv ~/cowbot/cowbot-manual /usr/share/cowbot/
-sudo mv ~/cowbot/img/dhenu.png /usr/share/cowbot/
+sudo mv ../cowbot-manual /usr/share/cowbot/
+sudo mv ../img/dhenu.png /usr/share/cowbot/
