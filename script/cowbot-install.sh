c43ebc38801de9064fd25a56d53b9cc1df86763e
Yash S Lakhtariya <98678102+yashslakhtariya@users.noreply.github.com>
2022-07-03 22:59:48 +0530

    Add files via upload

---
+++ script/cowbot-install.sh
@@ -0,0 +1,7 @@
+chmod +x ../src/cowbot
+chmod +x ../src/cowbot-help
+chmod +x ../src/cowbot-setup
+sudo cp ../src/* /usr/bin/
+sudo mkdir /usr/share/cowbot
+sudo cp ../cowbot-manual /usr/share/cowbot/
+sudo cp ../img/dhenu.png /usr/share/cowbot/
---
+++ script/cowbot-uninstall.sh
@@ -0,0 +1,4 @@
+sudo rm /usr/bin/cowbot*
+sudo rm -r /usr/share/cowbot/
+sudo pacman -R --noconfirm cowsay cmatrix sl asciiquarium fortune-mod
+paru -R --noconfirm xcowsay
